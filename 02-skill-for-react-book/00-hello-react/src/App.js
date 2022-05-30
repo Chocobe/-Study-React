8f77043ad7a26169ce779388619bbdcc55c7b97f
김영우 <kyw05171@gmail.com>
2022-05-30 21:57:01 +0900

    2장 JSX 실습 02

--- 02-skill-for-react-book/00-hello-react/src/App.js
+++ 02-skill-for-react-book/00-hello-react/src/App.js
@@ -4,9 +4,10 @@ const App = () => {
   const name = "리액트";
 
   return (
-    <div className="react">
-      {name}
-    </div>
+    <>
+      <div className="react">{name}</div>
+      <input />
+    </>
   );
 };
 
