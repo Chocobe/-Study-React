30fd1462ba1ed8552c0ef226fa69fc47a1795a50
김영우 <kyw05171@gmail.com>
2022-07-03 01:39:09 +0900

    17. 리덕스를 사용하여 리액트 애플리케이션 상태 관리하기 05

--- 02-skill-for-react-book/09-react-redux-tutorial/src/containers/CounterContainer.js
+++ 02-skill-for-react-book/09-react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,28 +1,63 @@
-import React from "react";
-import { connect } from "react-redux";
+// 02. useSelector(), useDispatch() 를 사용한 CounterContainer 만들기
+import React, {
+  useCallback,
+} from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({
-  number,
-  increase,
-  decrease,
-}) => {
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number);
+  
+  const dispatch = useDispatch();
+  
+  const onIncrease = useCallback(() => {
+    dispatch(increase());
+  }, [dispatch]);
+  
+  const onDecrease = useCallback(() => {
+    dispatch(decrease());
+  }, [dispatch]);
+    
   return (
     <Counter
       number={number}
-      onIncrease={increase}
-      onDecrease={decrease}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
     />
   );
 };
 
-export default connect(
-  state => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
+export default CounterContainer;
+
+
+
+// 01. connect() 를 사용한 CounterContainer 만들기
+// import React from "react";
+// import { connect } from "react-redux";
+// import Counter from "../components/Counter";
+// import { increase, decrease } from "../modules/counter";
+
+// const CounterContainer = ({
+//   number,
+//   increase,
+//   decrease,
+// }) => {
+//   return (
+//     <Counter
+//       number={number}
+//       onIncrease={increase}
+//       onDecrease={decrease}
+//     />
+//   );
+// };
+
+// export default connect(
+//   state => ({
+//     number: state.counter.number,
+//   }),
+//   {
+//     increase,
+//     decrease,
+//   },
+// )(CounterContainer);
--- 02-skill-for-react-book/09-react-redux-tutorial/src/containers/TodosContainer.js
+++ 02-skill-for-react-book/09-react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,46 +1,95 @@
-import React from "react";
-import { connect } from "react-redux";
+// 02. useSelector(), useDispatch() 를 사용한 TodosContainer 만들기
+import React, {
+  useCallback,
+} from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Todos from "../components/Todos";
-import {
-  changeInput,
-  toggle,
-  insert,
-  remove,
-} from "../modules/todos";
+import { changeInput, insert, toggle, remove } from "../modules/todos";
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  toggle,
-  insert,
-  remove,
-}) => {
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+
+  const dispatch = useDispatch();
+
+  const onChangeInput = useCallback(input => {
+    dispatch(changeInput(input));
+  }, [dispatch]);
+
+  const onInsert = useCallback(text => {
+    dispatch(insert(text));
+  }, [dispatch]);
+  
+  const onToggle = useCallback(id => {
+    dispatch(toggle(id));
+  }, [dispatch]);
+
+  const onRemove = useCallback(id => {
+    dispatch(remove(id));
+  }, [dispatch]);
+
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onToggle={toggle}
-      onInsert={insert}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onToggle={onToggle}
+      onInsert={onInsert}
+      onRemove={onRemove}
     />
   );
 };
 
-const mapStateToProps = state => ({
-  input: state.todos.input,
-  todos: state.todos.todos,
-});
+export default React.memo(TodosContainer);
 
-const mapDispatchToProps = dispatch => ({
-  changeInput: text => dispatch(changeInput(text)),
-  toggle: id => dispatch(toggle(id)),
-  insert: text => dispatch(insert(text)),
-  remove: id => dispatch(remove(id)),
-});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(TodosContainer);
+
+// 01. connect() 를 사용한 TodosContainer 만들기
+// import React from "react";
+// import { connect } from "react-redux";
+// import Todos from "../components/Todos";
+// import {
+//   changeInput,
+//   toggle,
+//   insert,
+//   remove,
+// } from "../modules/todos";
+
+// const TodosContainer = ({
+//   input,
+//   todos,
+//   changeInput,
+//   toggle,
+//   insert,
+//   remove,
+// }) => {
+//   return (
+//     <Todos
+//       input={input}
+//       todos={todos}
+//       onChangeInput={changeInput}
+//       onToggle={toggle}
+//       onInsert={insert}
+//       onRemove={remove}
+//     />
+//   );
+// };
+
+// const mapStateToProps = state => ({
+//   input: state.todos.input,
+//   todos: state.todos.todos,
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   changeInput: text => dispatch(changeInput(text)),
+//   toggle: id => dispatch(toggle(id)),
+//   insert: text => dispatch(insert(text)),
+//   remove: id => dispatch(remove(id)),
+// });
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps,
+// )(TodosContainer);
